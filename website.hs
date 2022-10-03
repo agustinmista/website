040fe8b39c34c22d3fec65f5f8afe7231edd2b7e
Agustin Mista <mista@chalmers.se>
2022-10-03 17:59:19 +0200

    Added atom.xml feed

--- website.hs
+++ website.hs
@@ -1,6 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import System.FilePath
+import System.Environment
+
+import Data.Maybe
 
 import Hakyll
 
@@ -8,90 +11,112 @@ import Hakyll
 -- Site builder
 
 main :: IO ()
-main = hakyllWith defaultConfiguration { providerDirectory = "website" } $ do
+main = do
+  url <- lookupEnvOrFail "WEBSITE_URL"
+  hakyllWith defaultConfiguration { providerDirectory = "website" } $ do
 
-  match "index.md" $ do
-    let ctx = defaultContext <> pubsContext <> postsContext
-    route (setExtension "html")
-    compile $ do
-      getResourceBody
-      >>= applyAsTemplate ctx
-      >>= renderPandoc
-      >>= loadAndApplyTemplate "templates/default.html" ctx
-      >>= relativizeUrls
-  
-  match "static/*" $ do
-    let ctx = defaultContext
-    route (topLevel `composeRoutes` niceRoute)
-    compile $ do
-      pandocCompiler
-      >>= loadAndApplyTemplate "templates/default.html" ctx
-      >>= relativizeUrls
+    match "index.md" $ do
+      let ctx = defaultContext <> pubsContext <> postsContext
+      route (setExtension "html")
+      compile $ do
+        getResourceBody
+        >>= applyAsTemplate ctx
+        >>= renderPandoc
+        >>= loadAndApplyTemplate "templates/default.html" ctx
+        >>= relativizeUrls
 
-  create ["pubs/index.html"] $ do
-    let ctx = defaultContext <> pubsContext
-    route idRoute
-    compile $ do
-      newItem 
-      >>= loadAndApplyTemplate "templates/pub-archive.html" ctx
-      >>= loadAndApplyTemplate "templates/default.html"     ctx
-      >>= relativizeUrls
-  
-  create ["teaching/index.html"] $ do
-    let ctx = defaultContext <> coursesContext
-    route idRoute
-    compile $ do
-      newItem
-      >>= loadAndApplyTemplate "templates/course-archive.html" ctx
-      >>= loadAndApplyTemplate "templates/default.html"        ctx
-      >>= relativizeUrls
+    create ["pubs/index.html"] $ do
+      let ctx = defaultContext <> pubsContext
+      route idRoute
+      compile $ do
+        newItem
+        >>= loadAndApplyTemplate "templates/pub-archive.html" ctx
+        >>= loadAndApplyTemplate "templates/default.html"     ctx
+        >>= relativizeUrls
 
-  create ["blog/index.html"] $ do
-    let ctx = defaultContext <> postsContext
-    route idRoute
-    compile $ do
-      newItem
-      >>= loadAndApplyTemplate "templates/post-archive.html" ctx
-      >>= loadAndApplyTemplate "templates/default.html"      ctx
-      >>= relativizeUrls
+    create ["teaching/index.html"] $ do
+      let ctx = defaultContext <> coursesContext
+      route idRoute
+      compile $ do
+        newItem
+        >>= loadAndApplyTemplate "templates/course-archive.html" ctx
+        >>= loadAndApplyTemplate "templates/default.html"        ctx
+        >>= relativizeUrls
 
-  match "pubs/*" $ version "pub" $ do
-    let ctx = defaultContext 
-    route (setExtension "html")
-    compile $ do
-      pandocCompiler
-      >>= loadAndApplyTemplate "templates/pub.html"     ctx
-      >>= loadAndApplyTemplate "templates/default.html" ctx
-      >>= relativizeUrls
-  
-  match "teaching/*" $ version "course" $ do
-    let ctx = defaultContext <> dateContext 
-    route (setExtension "html")
-    compile $ do
-      pandocCompiler
-      >>= loadAndApplyTemplate "templates/course.html"  ctx
-      >>= loadAndApplyTemplate "templates/default.html" ctx
-      >>= relativizeUrls
+    create ["blog/index.html"] $ do
+      let ctx = defaultContext <> postsContext
+      route idRoute
+      compile $ do
+        newItem
+        >>= loadAndApplyTemplate "templates/post-archive.html" ctx
+        >>= loadAndApplyTemplate "templates/default.html"      ctx
+        >>= relativizeUrls
 
-  match "blog/*" $ version "post" $ do
-    let ctx = defaultContext <> dateContext 
-    route (setExtension "html")
-    compile $ do
-      pandocCompiler
-      >>= loadAndApplyTemplate "templates/post.html"    ctx
-      >>= loadAndApplyTemplate "templates/default.html" ctx
-      >>= relativizeUrls
-  
-  match "assets/**/*" $ do
-    route idRoute
-    compile copyFileCompiler
+    create ["atom.xml"] $ do
+      let ctx = defaultContext <> postSnapshotsContext <> bodyField "description"
+      route idRoute
+      compile $ do
+        loadPostSnapshots
+        >>= renderAtom (atomConfig url) ctx
 
-  match "css/*" $ do
-    route idRoute
-    compile compressCssCompiler
+    match "static/*" $ do
+      let ctx = defaultContext
+      route (topLevel `composeRoutes` niceRoute)
+      compile $ do
+        pandocCompiler
+        >>= loadAndApplyTemplate "templates/default.html" ctx
+        >>= relativizeUrls
 
-  match "templates/*" $ do
-    compile templateBodyCompiler
+    match "pubs/*" $ version "pub" $ do
+      let ctx = defaultContext
+      route (setExtension "html")
+      compile $ do
+        pandocCompiler
+        >>= loadAndApplyTemplate "templates/pub.html"     ctx
+        >>= loadAndApplyTemplate "templates/default.html" ctx
+        >>= relativizeUrls
+
+    match "teaching/*" $ version "course" $ do
+      let ctx = defaultContext <> dateContext
+      route (setExtension "html")
+      compile $ do
+        pandocCompiler
+        >>= loadAndApplyTemplate "templates/course.html"  ctx
+        >>= loadAndApplyTemplate "templates/default.html" ctx
+        >>= relativizeUrls
+
+    match "blog/*" $ version "post" $ do
+      let ctx = defaultContext <> dateContext
+      route (setExtension "html")
+      compile $ do
+        pandocCompiler
+        >>= loadAndApplyTemplate "templates/post.html"    ctx
+        >>= saveSnapshot "content"
+        >>= loadAndApplyTemplate "templates/default.html" ctx
+        >>= relativizeUrls
+
+    match "assets/**/*" $ do
+      route idRoute
+      compile copyFileCompiler
+
+    match "css/*" $ do
+      route idRoute
+      compile compressCssCompiler
+
+    match "templates/*" $ do
+      compile templateBodyCompiler
+
+----------------------------------------
+-- Atom feed
+
+atomConfig :: String -> FeedConfiguration
+atomConfig url = FeedConfiguration {
+  feedTitle = "Agustín Mista",
+  feedDescription = "This feed provides the latests posts from my personal website",
+  feedAuthorName = "Agustín Mista",
+  feedAuthorEmail = "agustin@mista.me",
+  feedRoot = url
+}
 
 ----------------------------------------
 -- Loaders
@@ -101,6 +126,9 @@ loadPubs = loadAll ("pubs/*" .&&. hasVersion "pub")
 
 loadPosts :: Compiler [Item String]
 loadPosts = loadAll ("blog/*" .&&. hasVersion "post")
+
+loadPostSnapshots :: Compiler [Item String]
+loadPostSnapshots = loadAllSnapshots ("blog/*" .&&. hasVersion "post") "content"
 
 loadCourses :: Compiler [Item String]
 loadCourses = loadAll ("teaching/*" .&&. hasVersion "course")
@@ -118,11 +146,14 @@ dateContext = dateField "date" "%B %e, %Y"
 postsContext :: Context String
 postsContext = listField "posts" defaultContext (loadPosts >>= recentFirst)
 
+postSnapshotsContext :: Context String
+postSnapshotsContext = listField "post_snapshots" defaultContext (loadPostSnapshots >>= recentFirst)
+
 pubsContext :: Context String
 pubsContext = listField "pubs" defaultContext (loadPubs >>= recentFirst)
 
 coursesContext :: Context String
-coursesContext = listField "courses" defaultContext (loadCourses >>= recentFirst) 
+coursesContext = listField "courses" defaultContext (loadCourses >>= recentFirst)
 
 ----------------------------------------
 -- Helpers
@@ -131,8 +162,15 @@ newItem :: Monoid m => Compiler (Item m)
 newItem = makeItem mempty
 
 niceRoute :: Routes
-niceRoute = customRoute $ \ident -> 
+niceRoute = customRoute $ \ident ->
   takeDirectory (toFilePath ident) </> takeBaseName (toFilePath ident) </> "index.html"
 
 topLevel :: Routes
 topLevel = customRoute (takeBaseName . toFilePath)
+
+lookupEnvOrFail :: String -> IO String
+lookupEnvOrFail key = do
+  mb <- lookupEnv key
+  case mb of
+    Nothing -> error ("Missing environment variable: " <> key)
+    Just val -> return val
