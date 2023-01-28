6e83a7b3516f5a5b3b023e666b4c87968f513db3
agustinmista <agustinmista@users.noreply.github.com>
2023-01-28 13:43:25 +0000

    deploy: 7863cece30a695090e2f27b73a041948ab4f371f

--- js/index.js
+++ js/index.js
@@ -1,31 +1,28 @@
 "use strict";
 
-// Burger menu
-// Links are shown by default on wide windows
-const wideWindow = window.innerWidth >= 992;
-const burger = document.getElementById("burger");
-const links = document.getElementById("links");
-
-hideLinks();
-
-function showLinks() {
-  burger.style.color = "var(--secondary)"
-  links.style.display = "grid";
-}
-
-function hideLinks() {
-  links.style.display = "none";
-  burger.style.color = "var(--primary)"
-}
-
-function toggleLinks() {
-  links.style.display === "none" ? showLinks() : hideLinks();
-}
-
-burger.onclick = function () {
-  toggleLinks();
-}
-
 window.onload = function () {
+
+  // Burger menu
+  // Links are shown by default on wide windows
+  const wideWindow = window.innerWidth >= 992;
+  const burger = document.getElementById("burger");
+  const links = document.getElementById("links");
+
+  function showLinks() {
+    links.style.display = "grid";
+    burger.style.color = "var(--primary)"
+  }
+
+  function hideLinks() {
+    links.style.display = "none";
+    burger.style.color = "var(--secondary)"
+  }
+
+  burger.onclick = function () {
+    links.style.display === "none" ? showLinks() : hideLinks();
+  }
+
+  // Set the default layout depending on the window width
   wideWindow ? showLinks() : hideLinks();
+
 }
